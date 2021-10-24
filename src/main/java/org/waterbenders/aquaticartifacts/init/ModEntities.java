13ee5a9898a0efd936cd11b7a2c14d168d3b22f3
Ryan <redryma@icloud.com>
2021-10-24 13:19:18 -0400

    Fix error with HeavyBoat

--- src/main/java/org/waterbenders/aquaticartifacts/init/ModEntities.java
+++ src/main/java/org/waterbenders/aquaticartifacts/init/ModEntities.java
@@ -13,6 +13,6 @@ public class ModEntities
 {
     public static final DeferredRegister<EntityType<?>> ENTITIES = DeferredRegister.create(ForgeRegistries.ENTITIES, AquaticArtifacts.MOD_ID);
 
-    public static final EntityType<HeavyBoat> TYPE_HEAVY_BOAT = EntityType.Builder.of(HeavyBoat::new, EntityClassification.MISC).sized(1.0F, 0.5F).build("aquatic_artifacts:heavy_boat");
+    public static final EntityType<HeavyBoat> TYPE_HEAVY_BOAT = EntityType.Builder.of((EntityType.IFactory<HeavyBoat>) HeavyBoat::new, EntityClassification.MISC).sized(1.0F, 0.5F).build("aquatic_artifacts:heavy_boat");
     public static final RegistryObject<EntityType<HeavyBoat>> HEAVY_BOAT = ENTITIES.register("heavy_boat", () -> TYPE_HEAVY_BOAT);
 }
